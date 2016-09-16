0e47e6f55fa94f475c53e1e722e44d224489f1ed
Cyril Russo <boite.pour.spam@gmail.com>
2016-09-16 08:08:58 +0200

    Add support for via waypoint in the transit time widget

--- server/transittime.php
+++ server/transittime.php
@@ -15,6 +15,8 @@ class TransitTimeProvider implements ServiceProvider {
 	public $start_longitude;
 	public $end_latitude;
 	public $end_longitude;
+        public $via_latitude;
+        public $via_longitude;
 
 	function TransitTimeProvider() {
 		$this->apiKey = "";
@@ -24,6 +26,8 @@ class TransitTimeProvider implements ServiceProvider {
 		$this->start_longitude = 0;
 		$this->end_latitude = 5;
 		$this->end_longitude = 0;
+                $this->via_latitude = 0;
+                $this->via_longitude = 0;
 	}
 
     public function getTunables() {
@@ -33,6 +37,8 @@ class TransitTimeProvider implements ServiceProvider {
 			"slong"        => array("type" => "fnum", "display" => "From Longitude", "value" => $this->start_longitude),
 			"elat"         => array("type" => "fnum", "display" => "To Latitude", "value" => $this->end_latitude),
 			"elong"        => array("type" => "fnum", "display" => "To Longitude", "value" => $this->end_longitude),
+			"vlat"         => array("type" => "fnum", "display" => "Via Latitude", "value" => $this->via_latitude),
+			"vlong"        => array("type" => "fnum", "display" => "Via Longitude", "value" => $this->via_longitude),
 		);
 		return $out;
 	}
@@ -42,6 +48,8 @@ class TransitTimeProvider implements ServiceProvider {
 		$this->start_longitude = $v["slong"]["value"];
 		$this->end_latitude = $v["elat"]["value"];
 		$this->end_longitude = $v["elong"]["value"];
+		$this->via_latitude = $v["vlat"]["value"];
+		$this->via_longitude = $v["vlong"]["value"];
 	}
 
     public function shape() {
@@ -57,9 +65,10 @@ class TransitTimeProvider implements ServiceProvider {
     public function render() {
 		if ($this->apiKey == "") return sprintf('<svg width="%d" height="%d" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg>', $this->width, $this->height);
 		// Gather information from BingMap
-		$raw = file_get_contents(
-                        "http://dev.virtualearth.net/REST/V1/Routes?wp.0=".$this->start_latitude."%2C".$this->start_longitude."&wp.1=".$this->end_latitude."%2C".$this->end_longitude."&optmz=timeWithTraffic&key=".$this->apiKey
-		);
+                $url = $this->via_latitude ? 
+                        "http://dev.virtualearth.net/REST/V1/Routes?wp.0=".$this->start_latitude."%2C".$this->start_longitude."&wp.2=".$this->end_latitude."%2C".$this->end_longitude."&vwp.1=".$this->via_latitude.','.$this->via_longitude."&optmz=timeWithTraffic&key=".$this->apiKey :
+                        "http://dev.virtualearth.net/REST/V1/Routes?wp.0=".$this->start_latitude."%2C".$this->start_longitude."&wp.1=".$this->end_latitude."%2C".$this->end_longitude."&optmz=timeWithTraffic&key=".$this->apiKey;
+		$raw = file_get_contents($url);
 		if (strlen($raw) == 0) return sprintf('<svg width="%d" height="%d" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg>', $this->width, $this->height);
 
 		$array = json_decode($raw,true);
@@ -69,7 +78,7 @@ class TransitTimeProvider implements ServiceProvider {
 		$out = sprintf('<svg width="%d" height="%d" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                    <text text-anchor="start" x="0" y="%d" fill="black" style="font-size: %dpx; font-family: %s;">Transit time: %dmn</text>
                                    <text text-anchor="start" x="0" y="%d" fill="black" style="font-size: %dpx; font-family: %s;">Traffic impact: %dmn</text>
-				</svg>', $this->width, $this->height, $fontSize, $fontSize, "Arial", $transitTimeSec / 60.0, 2 * $fontSize, $fontSize, "Arial", ($transitTimeTrafficSec - $transitTimeSec) / 60);
+				</svg>', $this->width, $this->height, $fontSize, $fontSize, "Arial", $transitTimeTrafficSec / 60.0, 2 * $fontSize, $fontSize, "Arial", ($transitTimeTrafficSec - $transitTimeSec) / 60);
 		return $out;
 
 	}
